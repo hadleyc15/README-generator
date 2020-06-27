139eb94ec1fadf6347a75db1ec84db4760057adc
Chris Hadley <hadleyc15@yahoo.com>
2020-06-27 13:55:04 -0600

    added more format to the readme gereation file

--- Develop/index.js
+++ Develop/index.js
@@ -8,32 +8,32 @@ const questions = [
         type: "input",
         name: "title",
         message: 'What is the title of your project?'
-    }, 
+    },
     {
         type: "input",
         name: "description",
         message: "Please describe your project."
-    }, 
+    },
     {
         type: "input",
         name: "installation",
         message: 'What steps must a user take to install your project? Separate commands with " | " to print on separate lines.'
-    }, 
+    },
     {
         type: "input",
         name: "usage",
         message: "What is your project used for?",
-    }, 
-    {
-        type: "input",
-        name: "contributing",
-        message: "Please describe how others can contribute to this project."
     },
     {
         type: "input",
         name: "tests",
         message: "How are tests run for this project?",
-    }, 
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please describe how others can contribute to this project."
+    },
     {
         type: "list",
         name: "license",
@@ -42,9 +42,10 @@ const questions = [
             "Apache 2.0",
             "MIT",
             "GPL3",
-            "BSD 3 Clause"
+            "BSD 3 Clause",
+            "None"
         ]
-    }, 
+    },
     {
         type: "input",
         name: "gitHub",
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,55 @@
+const badges = {
+  "BSD 3 Clause": "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+  "GPL3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+};
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ##Table of Contents
+  
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+  
+  ${badges[data.license]}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+  
+  ## Questions
+
+    [${data.github}](https:://github.com/${data.github})
+
+    [${data.email}](mailto;${data.email})
+    
+    ${data.contact}
 `;
 }
 
